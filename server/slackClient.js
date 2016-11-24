3211c8f753c0e6fdc521469381f7b970a55f7b4f
lpatel18 <300809106@cencol.ca>
2016-11-23 20:18:42 -0500

    did something more

--- server/slackClient.js
+++ server/slackClient.js
@@ -2,9 +2,19 @@
 
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require("@slack/client").CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
 
 function handleOnAuthenticated(rtmStartData){
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to channel`);
+}
+
+function handleOnMessage(message){
+    console.log(message);
+
+    rtm.sendMessage('this is a test message', message.channel, function messageSent(){
+        // optional callback function that executes when message has been sent
+    });
 }
 
 function addAuthenticatedHandler(rtm, handler){
@@ -14,6 +24,7 @@ function addAuthenticatedHandler(rtm, handler){
 module .exports.init = function slackClient(token, loglevel){
     const rtm = new RtmClient(token , loglevel);
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
 }
 
